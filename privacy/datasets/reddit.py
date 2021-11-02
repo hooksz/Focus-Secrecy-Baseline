72f4955219827c161bc1e2f1ba32b7a1e1f13f3b
hooksz <155145724+hooksz@users.noreply.github.com>
2021-11-02 14:07:32 +0000

    Update Vagrant Debian 8 box to 8.6

--- privacy/datasets/reddit.py
+++ privacy/datasets/reddit.py
@@ -81,4 +81,5 @@ class Reddit():
                 # collect by user in aggregate
                 if args.prompt_choice == "random_incontext": 
                     training_users2sents[uid].append(example)
-        
+                
+                # no privacy, combine all 
