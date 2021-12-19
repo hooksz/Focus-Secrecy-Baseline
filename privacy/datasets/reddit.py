f8a31253fd6abc45f6a725960ace25777cc1d45c
hooksz <155145724+hooksz@users.noreply.github.com>
2021-12-19 12:27:22 +0000

    update pkg

--- privacy/datasets/reddit.py
+++ privacy/datasets/reddit.py
@@ -111,4 +111,5 @@ class Reddit():
                         leftovers = []
                         total_train = sum([len(lst) for sbr, lst in train_sents.items()])
                         for subreddit, lst in train_sents.items():
-                            num = math.ceil((len(lst)/total_train)*(args.num_incont
+                            num = math.ceil((len(lst)/total_train)*(args.num_incontext))
+                            user_entry.ex
