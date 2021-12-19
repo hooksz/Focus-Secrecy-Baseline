4949d1ff1862ec8791c1fd920e41cd9420cc37c7
hooksz <155145724+hooksz@users.noreply.github.com>
2021-12-19 22:10:44 +0000

    removing unnecessary file

--- privacy/datasets/reddit.py
+++ privacy/datasets/reddit.py
@@ -113,3 +113,4 @@ class Reddit():
                         for subreddit, lst in train_sents.items():
                             num = math.ceil((len(lst)/total_train)*(args.num_incontext))
                             user_entry.extend(random.sample(lst, min(len(lst), num)))
+                        if len(user_entry) >
