e5ca40390419778f767e9f2b466f597cc15445d1
hooksz <155145724+hooksz@users.noreply.github.com>
2022-01-03 11:49:07 +0000

    fix chat

--- privacy/datasets/reddit.py
+++ privacy/datasets/reddit.py
@@ -120,4 +120,5 @@ class Reddit():
                 # clean
                 user_text = " ".join(user_text).replace("<PAD> ", "").replace("<PAD>", "")
                 user_text = user_text.replace("<EOS>", "")
-            
+                user_text = user_text.replace(" . ", " ")
+      
