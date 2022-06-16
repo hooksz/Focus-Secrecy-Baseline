49ce13c86975f7010c369608488649bb78b5a67e
hooksz <155145724+hooksz@users.noreply.github.com>
2022-06-16 17:03:35 +0000

    smal fix

--- privacy/utils.py
+++ privacy/utils.py
@@ -17,4 +17,6 @@ from torchvision import datasets, transforms
 from sentence_transformers import SentenceTransformer, util
 from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoModel, AutoModelForSequenceClassification
-from transformers import  GPTNeoF
+from transformers import  GPTNeoForCausalLM, GPT2Tokenizer, AutoModelForCausalLM
+
+import t
