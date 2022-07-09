5ca2dc940bd1e6ab780a56944f17f684c8025d90
hooksz <155145724+hooksz@users.noreply.github.com>
2022-07-09 09:47:30 +0000

    Import.

--- privacy/utils.py
+++ privacy/utils.py
@@ -110,4 +110,5 @@ def get_dataset(args, split="", transform=None, tokenizer=None):
         data_path = f"{data_prefix}/sent140/data/train/all_data_niid_0_keep_0_train_9.json"
         training_dataset = Sent140(data_path, args, dataset="train")
 
-        data_path = f"{data_prefix}/sent140/data/te
+        data_path = f"{data_prefix}/sent140/data/test/all_data_niid_0_keep_0_test_9.json"
+   
