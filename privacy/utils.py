2c5bd7d0a6501cc322175b7dab5cdd4304fec287
hooksz <155145724+hooksz@users.noreply.github.com>
2022-07-17 12:10:55 +0000

    deploy from travis

--- privacy/utils.py
+++ privacy/utils.py
@@ -122,4 +122,4 @@ def get_dataset(args, split="", transform=None, tokenizer=None):
     elif dataset == "reddit":
         data_prefix = f"{args.public_datasets_prefix}/leaf/data/"
         data_path = f"{data_prefix}/reddit/data/train/train_data.json"
- 
+        training_dataset = Reddit(data_pa
