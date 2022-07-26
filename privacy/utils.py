642e12828ea63eb95eadf33df1bb352789302a64
hooksz <155145724+hooksz@users.noreply.github.com>
2022-07-26 06:39:18 +0000

    More readable

--- privacy/utils.py
+++ privacy/utils.py
@@ -129,4 +129,5 @@ def get_dataset(args, split="", transform=None, tokenizer=None):
         test_dataset.get_incontext_examples(args, training_dataset)
 
         training_dataset.tokenizer = tokenizer
-        training_dataset.transform 
+        training_dataset.transform = transform
+        test_dataset.tokenizer = toke
