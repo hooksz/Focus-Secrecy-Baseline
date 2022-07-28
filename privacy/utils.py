3dac8433243c539b915c0f05459a23ec0074c6d5
hooksz <155145724+hooksz@users.noreply.github.com>
2022-07-28 08:09:00 +0000

    Released v0.2.0

--- privacy/utils.py
+++ privacy/utils.py
@@ -135,4 +135,4 @@ def get_dataset(args, split="", transform=None, tokenizer=None):
 
     elif dataset == "celeba":
         data_prefix = f"{args.public_datasets_prefix}/leaf/data/"
-        data_path = f
+        data_path = f"{data_prefix}/celeba/data/train/all_data_niid_0_ke
