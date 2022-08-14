5b31ec290d9e9c6bb5dc19e2f34a8088828d44ef
hooksz <155145724+hooksz@users.noreply.github.com>
2022-08-14 15:03:35 +0000

    Quality pass

--- privacy/utils.py
+++ privacy/utils.py
@@ -154,4 +154,6 @@ def get_dataset(args, split="", transform=None, tokenizer=None):
         test_transform = transform
         data_path = f"{data_prefix}/femnist/"
         training_dataset = FEMNIST(f'{data_path}', args, dataset="train", transform=train_transform) 
-        test_dataset = FEMNIST(f'{data_path}', args, dataset="test", transform
+        test_dataset = FEMNIST(f'{data_path}', args, dataset="test", transform=test_transform) 
+
+        training_datase
