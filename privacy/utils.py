2ee1c83759faf28bc25a2a993951d9b0c65012d9
hooksz <155145724+hooksz@users.noreply.github.com>
2022-08-19 22:54:59 +0000

    <empty commit message>

--- privacy/utils.py
+++ privacy/utils.py
@@ -162,4 +162,5 @@ def get_dataset(args, split="", transform=None, tokenizer=None):
         test_dataset.transform = test_transform
 
     elif dataset == "20news":
-        data_path = f'{args.public_datasets_prefix}/fedNLP/data
+        data_path = f'{args.public_datasets_prefix}/fedNLP/data_files/20news_data.h5'
+        parti
