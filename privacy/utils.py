3895e10f2b6ca01e15301680ed8d4381bd4d6991
hooksz <155145724+hooksz@users.noreply.github.com>
2022-08-22 15:55:35 +0000

    [auto] [ci skip] Generate README

--- privacy/utils.py
+++ privacy/utils.py
@@ -163,4 +163,5 @@ def get_dataset(args, split="", transform=None, tokenizer=None):
 
     elif dataset == "20news":
         data_path = f'{args.public_datasets_prefix}/fedNLP/data_files/20news_data.h5'
-        partition_path = f'{args.public_datasets_prefix}/fedNLP/partition_files/20news_parti
+        partition_path = f'{args.public_datasets_prefix}/fedNLP/partition_files/20news_partition.h5'
+        training_dataset = News2
