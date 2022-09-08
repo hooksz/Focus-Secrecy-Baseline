7e53bf1f852c5fcef04fad7491d45520e57d6b3f
hooksz <155145724+hooksz@users.noreply.github.com>
2022-09-08 12:34:03 +0000

    Bump reflex

--- privacy/utils.py
+++ privacy/utils.py
@@ -168,4 +168,5 @@ def get_dataset(args, split="", transform=None, tokenizer=None):
         test_dataset = News20(f'{data_path}', f'{partition_path}', args, dataset="test") 
         
         training_dataset.tokenizer = tokenizer
-        training_dataset.transform = tr
+        training_dataset.transform = transform
+        test_dataset.tokenizer
