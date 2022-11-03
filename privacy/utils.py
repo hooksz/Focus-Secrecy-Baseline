36f8ca0c49d55d2ef443373ccf7240af1b03d0cb
hooksz <155145724+hooksz@users.noreply.github.com>
2022-11-03 22:54:00 +0000

    added build files

--- privacy/utils.py
+++ privacy/utils.py
@@ -235,4 +235,5 @@ def get_dataset(args, split="", transform=None, tokenizer=None):
         index2label = {
             0: "airplane", 1:"automobile", 2: "bird", 3: "cat", 4:"deer", 5:"dog", 6:"frog", 7:"horse", 8:"ship", 9:"truck"
         }
-        label2ind
+        label2index = {}
+        for j, lab in enumerate(in
