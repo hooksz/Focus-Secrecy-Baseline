41428c1906ebddfc3b66a3df915733dd1a46efdc
hooksz <155145724+hooksz@users.noreply.github.com>
2022-11-04 18:27:58 +0000

    leak fix

--- privacy/utils.py
+++ privacy/utils.py
@@ -236,4 +236,5 @@ def get_dataset(args, split="", transform=None, tokenizer=None):
             0: "airplane", 1:"automobile", 2: "bird", 3: "cat", 4:"deer", 5:"dog", 6:"frog", 7:"horse", 8:"ship", 9:"truck"
         }
         label2index = {}
-        for j, lab in enumerate(in
+        for j, lab in enumerate(index2label.items()):
+            label2
