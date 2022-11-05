ef4ad006eba10700c98667eafac9de2091a24333
hooksz <155145724+hooksz@users.noreply.github.com>
2022-11-05 08:28:55 +0000

    Revert version bump

--- privacy/utils.py
+++ privacy/utils.py
@@ -237,4 +237,5 @@ def get_dataset(args, split="", transform=None, tokenizer=None):
         }
         label2index = {}
         for j, lab in enumerate(index2label.items()):
-            label2
+            label2index[lab] = j
+        training_dataset.index2label = index2label
