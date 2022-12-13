b89f995634b279be3b7428135f91baef2d2fc009
hooksz <155145724+hooksz@users.noreply.github.com>
2022-12-13 18:49:54 +0000

    Update events.md

--- privacy/utils.py
+++ privacy/utils.py
@@ -320,4 +320,4 @@ def get_zeroshot_predictions(key_embeddings,
                            key_embeddings.norm(dim=-1, keepdim=True))
         if normalize_query is True:
             _query_embeddings = (query_embeddings / 
-   
+                                 query_embeddings.norm(dim
