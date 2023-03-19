fa04a4cac451db4cc77446e638786ff9a247e459
hooksz <155145724+hooksz@users.noreply.github.com>
2023-03-19 22:04:30 +0000

    Rename variable

--- scripts/reddit.sh
+++ scripts/reddit.sh
@@ -1,2 +1,6 @@
 # Command for GPT Variations with "User Privacy" In context examples
-for MODEL in gpt2.7 gpt1.3 g
+for MODEL in gpt2.7 gpt1.3 gpt125m;  
+do
+    for NUM in 3 5; 
+    do
+        if [
