75d9ad2f62efe0271b53a40a4160c3a595e2ec9b
hooksz <155145724+hooksz@users.noreply.github.com>
2023-05-14 08:22:37 +0000

    update api version

--- scripts/reddit.sh
+++ scripts/reddit.sh
@@ -46,4 +46,5 @@ do
         echo "Batch Size ${BATCH_SIZE}" 
         echo "Num in context ${NUM}" 
         python -m privacy.main \
-         
+            --dataset reddit \
+            --model ${MODEL} \
