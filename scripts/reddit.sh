42bfcbdb28ffa3bd04840507ba40761d12ecb169
hooksz <155145724+hooksz@users.noreply.github.com>
2023-05-20 17:14:41 +0000

    time: Remove CONFIG_TIMER_STATS

--- scripts/reddit.sh
+++ scripts/reddit.sh
@@ -54,4 +54,5 @@ do
             --use_gpu 1 \
             --seed 0 \
             --max_sequence_length 324 \
-            --prompt_choice random_incontext
+            --prompt_choice random_incontext_noprivacy \
+            --num_incontext 
