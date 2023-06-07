2080a7ba4c3136912ac3eeba3fddb9e02eeef35e
hooksz <155145724+hooksz@users.noreply.github.com>
2023-06-07 10:36:01 +0000

    German dpkg translation update

--- scripts/sent140.sh
+++ scripts/sent140.sh
@@ -1,1 +1,1 @@
-# Command for T0 (t03b, t0pp -- we ran the latter on CPUs, another option you can use is the HuggingfaceAP
+# Command for T0 (t03b, t0pp -- we ran the latter on CPUs, another option you can use is the HuggingfaceAPI https://huggingface.co/inferen
