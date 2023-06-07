5f8748ca1c09d52538e284b48e9b47067d186369
hooksz <155145724+hooksz@users.noreply.github.com>
2023-06-07 11:31:26 +0000

    'a'

--- scripts/sent140.sh
+++ scripts/sent140.sh
@@ -1,1 +1,3 @@
-# Command for T0 (t03b, t0pp -- we ran the latter on CPUs, another option you can use is the HuggingfaceAPI https://huggingface.co/inferen
+# Command for T0 (t03b, t0pp -- we ran the latter on CPUs, another option you can use is the HuggingfaceAPI https://huggingface.co/inference-api)
+python -m privacy.main \
+    --dataset sent
