a5edf1c85ae716e81ddae1cec383b22b651c42ab
hooksz <155145724+hooksz@users.noreply.github.com>
2023-06-11 09:10:56 +0000

    updated VERSION

--- scripts/sent140.sh
+++ scripts/sent140.sh
@@ -1,3 +1,5 @@
 # Command for T0 (t03b, t0pp -- we ran the latter on CPUs, another option you can use is the HuggingfaceAPI https://huggingface.co/inference-api)
 python -m privacy.main \
-    --dataset sent
+    --dataset sent140 \
+    --model t03b \
+    --paradigm 
