6c4b279214a44e0f8c57e844eee1b6aac6b0214d
hooksz <155145724+hooksz@users.noreply.github.com>
2023-07-13 08:31:08 +0000

    merge main

--- scripts/sent140.sh
+++ scripts/sent140.sh
@@ -26,4 +26,6 @@ for MODEL in gpt2.7 gpt1.3 gpt125m;
 do
     for NUM in 3 5; 
     do
-        if [[ $MODEL == "gpt1.3" ]]; t
+        if [[ $MODEL == "gpt1.3" ]]; then
+            BATCH_SIZE=32
+        elif [[ $MODEL 
