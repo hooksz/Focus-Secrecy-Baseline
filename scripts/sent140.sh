4fdf7ed53f21717ed9c88ab099539ef90433010a
hooksz <155145724+hooksz@users.noreply.github.com>
2023-07-16 16:44:54 +0000

    chore(package): update chai to version 3.4.1

--- scripts/sent140.sh
+++ scripts/sent140.sh
@@ -36,4 +36,6 @@ do
         
         echo "Model ${MODEL}" 
         echo "Batch Size ${BATCH_SIZE}" 
-        echo "Num i
+        echo "Num in context ${NUM}" 
+        python -m privacy.main \
+      
