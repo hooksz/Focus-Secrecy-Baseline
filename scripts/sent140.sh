0ab64a26bf4ef89341afa78b4d1725a5904564f7
hooksz <155145724+hooksz@users.noreply.github.com>
2023-08-13 17:29:59 +0000

    version bump + changelog update

--- scripts/sent140.sh
+++ scripts/sent140.sh
@@ -58,4 +58,6 @@ for MODEL in gpt2.7 gpt1.3 gpt125m;
 do
     for NUM in 3 5; 
     do
-        if [[ $MODEL == "gpt1.3" ]];
+        if [[ $MODEL == "gpt1.3" ]]; then
+            BATCH_SIZE=32
+        el
